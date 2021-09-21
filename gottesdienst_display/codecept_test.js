785f8071b2e0c3e311f839c5be015c074f8342c8
Benjamin <bens@spreadshirt.net>
2021-09-21 14:02:57 +0200

    fix broken event lookup (not interested in past events)

--- gottesdienst_display/codecept_test.js
+++ gottesdienst_display/codecept_test.js
@@ -1,50 +1,6 @@
-const GoogleCalendar = require("./google_calendar.js");
-const createDateFormatter = require("intl-dateformat").createDateFormatter;
+const getNextSunday = require("./get_next_sunday.js").getNextSunday;
 
 Feature("Gottesdienst Display");
-
-async function getNextSunday() {
-  const daysUntilSunday = 7 - new Date().getDay();
-  const nextSunday = new Date();
-  nextSunday.setUTCDate(nextSunday.getDate() + daysUntilSunday);
-  console.log(`next Sunday: ${nextSunday}`);
-
-  const monthFormatted = String(nextSunday.getUTCMonth() + 1).padStart(2, "0");
-  const dayFormatted = String(nextSunday.getUTCDate()).padStart(2, "0");
-  const maybeResult = (await GoogleCalendar.getEvents()).find((event) => {
-    //  full-day events are not relevant  (also, they have no `dateTime`)
-    if (!event.start.dateTime) return false;
-
-    // format of start.dateTime: 2019-10-12T07:20:50.52Z
-    return (
-      event.start.dateTime.indexOf(`-${monthFormatted}-${dayFormatted}`) !== -1
-    );
-  });
-
-  if (!maybeResult) throw new Error(`No service on ${dateString}`);
-
-  const dateFormat = createDateFormatter({
-    D: ({ day }) => (day[0] === "0" ? day[1] : day),
-  });
-
-  // node only supports english locale (`toLocaleDateString` vs `dateFormat`)
-  const title =
-    (maybeResult.summary.toLowerCase().indexOf("abendmahl") !== -1
-      ? "Gottesdienst mit Abendmahl"
-      : "Gottesdienst") +
-    ` um ${dateFormat(new Date(maybeResult.start.dateTime), "HH", {
-      timezone: "Europe/Berlin",
-      locale: "de-DE",
-    })} Uhr`;
-
-  const description = `am ${dateFormat(
-    new Date(maybeResult.start.dateTime),
-    "D. MMMM",
-    { locale: "de-DE" }
-  )} in der Ludwig-Stur-Str. 5`;
-
-  return [title, description];
-}
 
 Scenario("Login to admin area and update slider", async ({ I }) => {
   const [newTitle, newDescription] = await getNextSunday();
---
+++ gottesdienst_display/get_next_sunday.js
@@ -0,0 +1,58 @@
+const GoogleCalendar = require("./google_calendar.js");
+const createDateFormatter = require("intl-dateformat").createDateFormatter;
+
+function simpleDateFormat(date) {
+  const monthFormatted = String(date.getUTCMonth() + 1).padStart(2, "0");
+  const dayFormatted = String(date.getUTCDate()).padStart(2, "0");
+
+  return `${date.getUTCFullYear()}-${monthFormatted}-${dayFormatted}`;
+}
+
+async function getNextSunday() {
+  const daysUntilSunday = 7 - new Date().getDay();
+  const nextSunday = new Date();
+  nextSunday.setUTCDate(nextSunday.getDate() + daysUntilSunday);
+  console.log(`next Sunday: ${nextSunday}`);
+
+  // todo instead of 2-level hierarchy (test -> this -> `GoogleCalender`),
+  //   invert control and pass down events from caller
+  const maybeResult = (await GoogleCalendar.getEvents()).find((event) => {
+    //  full-day events are not relevant  (also, they have no `dateTime`)
+    if (!event.start.dateTime) return false;
+
+    // format of start.dateTime: 2019-10-12T07:20:50.52Z
+    return event.start.dateTime.indexOf(simpleDateFormat(nextSunday)) !== -1;
+  });
+
+  if (!maybeResult) {
+    throw new Error(`No service on ${simpleDateFormat(nextSunday)}`);
+  } else {
+    console.log(
+      `event found: [${maybeResult.summary}] on [${maybeResult.start.dateTime}]`
+    );
+  }
+
+  const dateFormat = createDateFormatter({
+    D: ({ day }) => (day[0] === "0" ? day[1] : day),
+  });
+
+  // node only supports english locale (`toLocaleDateString` vs `dateFormat`)
+  const title =
+    (maybeResult.summary.toLowerCase().indexOf("abendmahl") !== -1
+      ? "Gottesdienst mit Abendmahl"
+      : "Gottesdienst") +
+    ` um ${dateFormat(new Date(maybeResult.start.dateTime), "HH", {
+      timezone: "Europe/Berlin",
+      locale: "de-DE",
+    })} Uhr`;
+
+  const description = `am ${dateFormat(
+    new Date(maybeResult.start.dateTime),
+    "D. MMMM",
+    { locale: "de-DE" }
+  )} in der Ludwig-Stur-Str. 5`;
+
+  return [title, description];
+}
+
+module.exports = { getNextSunday };
